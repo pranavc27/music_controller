949c7ef11a4e4a4917f722c27ad6920c7bf20812
Pranav <pranav.chandak03@gmail.com>
2024-04-17 12:47:22 +0530

    setting up react and django and rendering first component

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+export default function App(props)
+{
+    return <h1>Testing React Code</h1>
+}
+
+const appDiv = document.getElementById("app");
+render (<App /> , appDiv);
